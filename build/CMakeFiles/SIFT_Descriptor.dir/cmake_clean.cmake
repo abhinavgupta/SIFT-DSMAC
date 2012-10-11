fe6aa053650be1d9b577de7a67edee766078c46c
Abhinav Gupta <aag999in@gmail.com>
2012-10-11 18:41:10 +0200

    updated new images

---
+++ build/CMakeFiles/SIFT_Descriptor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SIFT_Descriptor.dir/SIFT_Descriptor.o"
+  "SIFT_Descriptor.pdb"
+  "SIFT_Descriptor"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SIFT_Descriptor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
